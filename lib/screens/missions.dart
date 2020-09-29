2b83587251fd45c01ab2178f20373a96f32fb791
Abdelrahman Negm <bodinegem@gmail.com>
2020-09-29 21:07:10 +0300

    v0.13

--- lib/screens/missions.dart
+++ lib/screens/missions.dart
@@ -16,7 +16,8 @@ class MissionsContainerScreen extends StatelessWidget {
 
   Widget topBar(BuildContext context){
     return Container(
-      height: MediaQuery.of(context).size.height/7,
+      margin: EdgeInsets.only(bottom: 60),
+      height: MediaQuery.of(context).size.height/6,
       width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
         color: Colors.white,
@@ -32,18 +33,61 @@ class MissionsContainerScreen extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          progressCircle(22.923423235),
+          /* Main mission text*/
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Main', style: TextStyle(fontSize: 18),),
+                Text('Mission', style: TextStyle(fontSize: 18),),
+              ],
+            ),
+          ),
+          /* progress circle */
+          progressCircle(66),
+          /* total days + completed days*/
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Total 30 days', style: TextStyle(fontSize: 10),),
+                Text('20', style: TextStyle(fontSize: 22, color: Colors.blueAccent, fontWeight: FontWeight.bold),),
+              ],
+            ),
+          ),
       ],)
+    );
+  }
+  Widget shortTermGoals(BuildContext context, Color priorityColor, String title){
+    return Container(
+      height: MediaQuery.of(context).size.height/10,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: .1,
+            blurRadius: 8,
+            offset: Offset(0, 8), // changes position of shadow
+          ),
+        ],
+      ),
     );
   }
 
   Widget progressCircle(double percent){
     int percentPrint =  percent.toInt(); // casts percent to int for display
     return CircularPercentIndicator(
+      circularStrokeCap: CircularStrokeCap.round,
       radius: 80.0,
-      lineWidth: 7.0,
+      lineWidth: 8.0,
       percent: percent/100,
-      center: new Text('$percentPrint%'),
+      //animation: true,
+      //animationDuration: 750,
+      center: new Text('$percentPrint%', style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
       progressColor: Colors.blueAccent,
     );
   }
